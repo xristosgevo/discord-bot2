85b7802ee985b6c163139e177cb977e8c02b6dee
xristosgevo <xristosgevo11@gmail.com>
2025-06-29 19:23:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
+import discord
+import os
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Bot ist online als {client.user}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "!ping":
+        await message.channel.send("Pong!")
+
+client.run(os.getenv("TOKEN"))
 
